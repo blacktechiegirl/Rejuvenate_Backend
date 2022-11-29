b681d4c892115f31ababb1b5c6d70cdca461ddf6
Aminat <amibarbs01@gmail.com>
2022-11-29 16:29:19 +0100

    service name

--- src/functions/generateS3Url/handler.ts
+++ src/functions/generateS3Url/handler.ts
@@ -1,22 +1,14 @@
-import * as dotenv from 'dotenv'
 import {S3} from 'aws-sdk'
 import { middyfy } from '@libs/lambda'
 import { formatJSONResponse } from "@libs/api-gateway";
 import { v4 } from "uuid";
 
-
-
-dotenv.config()
-
 const bucketName = "rejuvenatebucket2022"
-
-
 const s3 = new S3({
   signatureVersion: 'v4'
 })
 
-export const generateUploadURL =middyfy( async(event)=> {
-  const imagename = event.pathParameters.imagename
+export const generateUploadURL =middyfy( async()=> {
   const uid = v4()
   const params = ({
     Bucket: bucketName,
@@ -25,6 +17,5 @@ export const generateUploadURL =middyfy( async(event)=> {
   })
   
   const uploadURL = await s3.getSignedUrlPromise('putObject', params)
-  console.log(imagename)
   return formatJSONResponse({uploadURL})
 })
