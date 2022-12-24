8c1e114ef006eab8639f4fa1f50db385fae6d515
Aminat <amibarbs01@gmail.com>
2022-12-24 21:14:58 +0100

    cart-table

--- src/functions/index.ts
+++ src/functions/index.ts
@@ -1,4 +1,4 @@
 export {getAllProducts, getProductByCategory, createProduct, getProductById } from './productfunction'
 export {generateUploadURL} from './generateS3Url'
-export {getAllCartItems, deleteCartItem,createCartItem  } from './cartfunction'
+// export {getAllCartItems, deleteCartItem,createCartItem  } from './cartfunction'
 export {getAllWishlistItems, createWishlistItem,deleteWishlistItem  } from './wishlistfunction'
